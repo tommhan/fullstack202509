a688a4dd5b494f2cbbaa9a28d21d584c4949d64a
hatom <tommhan@gmail.com>
2025-09-29 16:54:07 +0300

    Komponentit lisätty ja ne saavat tiedot propsien kautta.

--- Osa1/kurssitiedot/src/App.jsx
+++ Osa1/kurssitiedot/src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const Header = (props) => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>{props.course}</h1>
+    </div>
   )
 }
+const Content = (props) => {
+  return ( <div>
+    <p>{props.part} {props.exercises}</p>
+  </div>)
+  
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p> Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    </div>
+  )
+}
+const App = () => {
+  
+
+  return (
+    <div>
+      <Header course = 'Half Stack application development'/>
+      <Content part = 'Fundamentals of React' exercises = {10} />
+      <Content part = 'Using props to pass data' exercises = {7} />
+      <Content part = 'State of a component' exercises = {14} />
+      <Total exercises1 = {10} exercises2 = {7} exercises3 = {14}/>
+    </div>
+  )
+}
+
 
 export default App
