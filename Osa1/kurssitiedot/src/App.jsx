dbe78dc14197af71fa3bd96430f61b6ad161e7df
Hannu Tommola <tommhan@gmail.com>
2026-07-02 17:56:04 +0300

    toimiiko

--- Osa1/kurssitiedot/src/App.jsx
+++ Osa1/kurssitiedot/src/App.jsx
@@ -20,16 +20,28 @@ const Total = (props) => {
     </div>
   )
 }
+const now = new Date()
+const kello = now.toLocaleTimeString('fi-FI', { hour: '2-digit', minute: '2-digit' })
+const paiva = now.toLocaleDateString('fi-FI')
+const aloitus = new Date(2026,5,24,11,0)
+const kulunutAikams = now - aloitus
+const erotusmin = Math.floor(kulunutAikams / (1000 * 60))
+const tunnit = Math.floor(erotusmin/60)
+const minuutit = erotusmin % 60
+
 const App = () => {
   
-
+console.log('toimiiko ääkköset')
   return (
     <div>
       <Header course = 'Half Stack application development'/>
+      
       <Content part = 'Fundamentals of React' exercises = {10} />
       <Content part = 'Using props to pass data' exercises = {7} />
       <Content part = 'State of a component' exercises = {14} />
       <Total exercises1 = {10} exercises2 = {7} exercises3 = {14}/>
+      <h3>Nyt on {paiva} ja kello on {kello}</h3>
+      <p>Aikaa on kulunut aloituksesta {tunnit} tuntia ja {minuutit} minuuttia</p>
     </div>
   )
 }
