666ecf2f9e3386752cf90120bac4b7a1c8dc0a5a
Hannu Tommola <tommhan@gmail.com>
2026-07-28 17:59:05 +0300

    Tehty olioliteraali kursseista.

--- Osa1/kurssitiedot/src/App.jsx
+++ Osa1/kurssitiedot/src/App.jsx
@@ -1,50 +1,52 @@
+const Header = ({ course }) => <h1>{course}</h1>
 
-const Header = (props) => {
-  return (
-    <div>
-      <h1>{props.course}</h1>
-    </div>
-  )
-}
-const Content = (props) => {
-  return ( <div>
-    <p>{props.part} {props.exercises}</p>
-  </div>)
-  
+const Content = ({ part, exercises }) => (
+  <div>
+    <p>{part} {exercises}</p>
+  </div>
+)
+
+/*const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
+*/
+const Total = ({ parts }) => {
+let total = 0;
+parts.forEach(part => {
+  total= total + part.exercises ;
+})
+  return <p>Number of exercises {total}</p>
 }
 
-const Total = (props) => {
-  return (
-    <div>
-      <p> Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
-    </div>
-  )
-}
 const now = new Date()
 const kello = now.toLocaleTimeString('fi-FI', { hour: '2-digit', minute: '2-digit' })
 const paiva = now.toLocaleDateString('fi-FI')
-const aloitus = new Date(2026,5,24,11,0)
+const aloitus = new Date(2026, 5, 24, 11, 0)
 const kulunutAikams = now - aloitus
 const erotusmin = Math.floor(kulunutAikams / (1000 * 60))
-const tunnit = Math.floor(erotusmin/60)
+const tunnit = Math.floor(erotusmin / 60)
 const minuutit = erotusmin % 60
+const paivat = Math.floor(tunnit / 24)
+const tunnitJaljella = tunnit % 24
 
 const App = () => {
-  
-console.log('toimiiko ääkköset')
+  const course = 'Half Stack application development'
+  const parts = [
+    { name: 'Fundamentals of React', exercises: 10 },
+    { name: 'Using props to pass data', exercises: 7 },
+    { name: 'State of a component', exercises: 14 }
+  ]
+
   return (
     <div>
-      <Header course = 'Half Stack application development'/>
-      
-      <Content part = 'Fundamentals of React' exercises = {10} />
-      <Content part = 'Using props to pass data' exercises = {7} />
-      <Content part = 'State of a component' exercises = {14} />
-      <Total exercises1 = {10} exercises2 = {7} exercises3 = {14}/>
+      <Header course={course} />
+      <Content part={parts[0].name} exercises={parts[0].exercises} />
+      <Content part={parts[1].name} exercises={parts[1].exercises} />
+      <Content part={parts[2].name} exercises={parts[2].exercises} />
+      <Total parts={parts} />
       <h3>Nyt on {paiva} ja kello on {kello}</h3>
-      <p>Aikaa on kulunut aloituksesta {tunnit} tuntia ja {minuutit} minuuttia</p>
+      <p>Aikaa on kulunut aloituksesta {paivat} päivää, {tunnitJaljella} tuntia ja {minuutit} minuuttia</p>
     </div>
   )
 }
-
 
 export default App
